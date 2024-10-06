79162859fa375428a6224b7b2d20baa4e000f97e
hndrk <hndrkrms@gmail.com>
2024-10-06 11:08:16 -0400

    lo que sea fin

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: 'https://hndrkm.github.io/Travel_OrreryTraveler',
   server: {
     port: 3001,
     host: '0.0.0.0',
